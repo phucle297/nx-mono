0f74a2b406ee9e43d6b4e0af80aa90ec130bfa3c
Le Dam Duy Phuc <permees@nexondv.com>
2025-02-12 11:27:15 +0700

    feat(ec-api-gateway): init ec-api-gateway

--- apps/ec-api-gateway/src/controllers/v1/product.controller.ts
+++ apps/ec-api-gateway/src/controllers/v1/product.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { CreateProductCommand } from '@ec-application';
 
