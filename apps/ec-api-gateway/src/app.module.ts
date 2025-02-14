ca1713c372f2f28dfccf2e75aa465e1aa2fb1f6a
Le Dam Duy Phuc <permees@nexondv.com>
2025-02-14 16:55:42 +0700

    feat(ec-api-gateway): add pino log'

--- apps/ec-api-gateway/src/app.module.ts
+++ apps/ec-api-gateway/src/app.module.ts
@@ -1,9 +1,20 @@
 import { ProductSDKModule } from '@ec-sdk'
 import { Module } from '@nestjs/common'
 import { ProductModule } from './v1/product/product.module'
+import { LoggerModule } from 'nestjs-pino'
 
 @Module({
   imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true
+          }
+        }
+      }
+    }),
     ProductModule,
     //SDK
     ProductSDKModule
